7215e218d3f29a6e9fc00916c50b29b1105e84a1
Nikki_Moscow <nikk178@mail.ru>
2016-04-13 08:55:22 +0300

    Update LAB4.cpp

--- LAB4.cpp
+++ LAB4.cpp
@@ -1,149 +1,205 @@
 #include "stdafx.h"
 #include <iostream>
+#include <math.h>
 
 using namespace std;
-class compx 
+
+class compx
 {
 private:
-	int di ,mn; // di - целая , mn - мнимая
-public :
-	int get_di(){
-	return di;
+	int di, mn; // di - целая , mn - мнимая
+public:
+	int get_di() {
+		return di;
 	}
-	int get_mn(){
-	return mn;
+	int get_mn() {
+		return mn;
 	}
 	compx() {};
-	compx (int d) {
-	di = d;
-	mn=0;
+	compx(int d) {
+		di = d;
+		mn = 0;
 	}
-	compx (int d, int m) {
-	di=d;
-	mn=m;
+	compx(int d, int m) {
+		di = d;
+		mn = m;
 	}
 	compx(compx &i) { // конструктор copy
-		di=i.di;
-		mn=i.mn;
+		di = i.di;
+		mn = i.mn;
 	}
-	~compx(){}
+	~compx() {}
 
-	compx & operator=  (compx &i){ // перегрузка присваивания
-	di=i.di;
-	mn=i.mn;
-	return (*this);
+	compx & operator=  (compx &i) { // перегрузка присваивания
+		di = i.di;
+		mn = i.mn;
+		return (*this);
 	}
 	compx compx :: operator * (compx &i)// перегрузка произведения 
 	{
-	compx tm;
-		tm.di=di*i.di;
-		tm.mn=di*i.mn;
+		compx tm;
+		tm.di = di*i.di;
+		tm.mn = di*i.mn;
 		return tm;
 	}
 	compx compx :: operator + (compx &i) // перегрузка сложения
 	{
-	compx tm;
-		tm.di=di+i.di;
-		tm.mn=di+i.mn;
+		compx tm;
+		tm.di = di + i.di;
+		tm.mn = di + i.mn;
 		return tm;
 	}
 };
-int main()
-{
-	compx a(1,10);
-	compx b(2,12);
-	cout << a.get_di() <<endl;
-	cout << a.get_mn() <<endl;
-	cout << b.get_di() <<endl;
-	cout << b.get_mn() <<endl;
-	cout << a.get_mn() + b.get_mn() <<endl;
-	cout << a.get_mn() * b.get_mn() <<endl;
-	system ("pause");
-	return 0;
-}
 
-/* Реализация действий с матрицами. 
-#include "stdafx.h"
-#include <iostream>
-using namespace std;
-class matr {
-	//private: 
-		int **matrica;
-		int n,m;
-	public :
-		matr(){
-		matrica=0;
-		m=0; n=0;
-		}
-		matr(int na,int mb)       
+class matrix {
+	compx** A;
+	int M;
+	int N;
+public:
+	matrix(int m, int n) : A(NULL), M(m), N(n)
 	{
-		n = na;
-		m = mb;
-		int **matrica = new int *[n];
-		for (int i = 0; i < n; i++)
+		A = new compx*[M]; //строки в массиве
+		for (int i = 0; i < M; i++)
 		{
-			matrica[i] = new int[m];
-			for (int j=0; j < m; j++){
-			matrica[i][j]=0;
-			}
+			A[i] = new compx[N]; //  столбцы
+			for (int j = 0; j < N; j++)
+				A[i][j] = compx(0, 0);
 		}
-		}
-		void showData()         //вывод данных массива на экран
+	}
+	compx& operator () (int i, int j) // перегрузка оператора обращение к элементу ()
 	{
-		for (int i = 0; i < n; i++)
-		{
-			for (int j=0; j<m; j++)
-			cout << matrica[i][j] << " | ";
-		}
-		cout << endl << endl;
+		return A[i][j];
 	}
 
-		
-		int proizv (int **aArr , int **bArr) { // Произведение матриц
-		int i,j;
-		int **cArr = new int *[i];
-		for (i = 0; i < m; i++)
-		cArr[i] = new int [j];
-		for(i = 0; i < m; i++) {
-			for(j = 0; j < m; j++)
+	friend matrix operator+(matrix& s1, matrix& s2);
+	friend matrix operator*(matrix& s1, matrix& s2);
+	friend matrix operator~(matrix& s1);
+
+	void print()
+	{
+		for (int i = 0; i < M; i++)
+		{
+			for (int j = 0; j < N; j++)
+				cout << A[i][j].get_di() << ", " << A[i][j].get_mn() << " | ";
+			cout << endl;
+		}
+		cout << endl;
+	}
+
+
+	~matrix()
+	{
+
+	}
+
+};
+
+matrix operator+(matrix& M1, matrix& M2)//перегрузка сложения матриц
+{
+	matrix L(M1.M, M1.N);
+	for (int i = 0; i < M1.M; i++)
+	{
+		for (int j = 0; j < M1.N; j++)
+		{
+			L(i, j) = M2(i, j) + M1(i, j);
+		}
+	}
+	return L;
+}
+
+matrix operator*(matrix& M1, matrix& M2)//перегрузка перемножения матриц
+{
+	if (M1.N != M2.M)
+		throw "dont true size";
+	matrix Q(M1.M, M1.N);
+
+	for (int i = 0; i < M1.M; i++)
+	{
+		for (int j = 0; j < M1.N; j++)
+		{
+			for (int t = 0; t < M1.N; t++)
 			{
-			cArr[i][j] = 0;
-			for(int k = 0; k <= m + 1; k++)
-			cArr[i][j] += (aArr[i][k] * bArr[k][j]);
+				Q(i, j) = Q(i, j) + (M1(i, t) * M2(t, j));
 			}
 		}
-		return **cArr;
-		}
-		int summ (int **aArr , int **bArr){ // Сумма матриц
-		int i,j;
-		int **cArr = new int *[i];
-		for (i = 0; i < m; i++)
-		cArr[i] = new int [j];
-		for(int i=0;i<n;i++){
-		for(int j=0;j<m;j++)
-    cArr[i][j] = aArr[i][j] + bArr[i][j];
-		}
-		return **cArr;
-		}
-		
-		int& operator ()(size_t i, size_t j) // Перегрузка оператора обращения по индексам
+	}
+	return Q;
+}
+
+matrix operator~(matrix& M1)//перегрузка транспонирования матрицы
+{
+	matrix L(M1.N, M1.M);
+	for (int i = 0; i < M1.M; i++)
+	{
+		for (int j = 0; j < M1.N; j++)
 		{
-			return matrica[i][j];
+			L(j, i) = M1(i, j);
 		}
-};
+	}
+	return L;
+}
 
 int main()
 {
+	compx value1(1, 7);
+	compx value2(6, -2);
 
-	matr ArrayInt(2,2);    //создаем объект класса и записываем в него данные
-	ArrayInt(0,0) = 1; //перегруженный ()
-	ArrayInt(0,1) = 2;
-	ArrayInt(1,0) = 3;
-	ArrayInt(1,1) = 4;
-	cout << "Массив ArrayInt:  ";
-	//ArrayInt.showData();
+	matrix A(3, 2);        // До запятой целай часть числа , а потом мнимая
+	A(0, 0) = compx(2, 3);
+	A(0, 1) = compx(1, 1);
+	A(1, 0) = compx(0, 1);
+	A(1, 1) = compx(3, 4);
+	A(2, 0) = compx(0, 0);
+	A(2, 1) = compx(1, 3);
+
+	matrix B(2, 2);
+	B(0, 0) = compx(1, 5);
+	B(0, 1) = compx(3, 3);
+	B(1, 0) = compx(6, 1);
+	B(1, 1) = compx(1, 0);
+
+	matrix X(3, 2);
+	X(0, 0) = compx(2, 2);
+	X(0, 1) = compx(1, 3);
+	X(1, 0) = compx(7, 3);
+	X(1, 1) = compx(2, 1);
+	X(2, 0) = compx(0, 0);
+	X(2, 1) = compx(1, 1);
+
+	cout << "Matrica A: " << endl;
+	A.print();
+	cout << "Matrica B: " << endl;
+	B.print();
+	cout << "Matrica X: " << endl;
+	X.print();
+	matrix C(2, 2);
+	C = A + X;//сложение матриц
+	cout << "Matrica A+F: " << endl;
+	C.print();
+	matrix O(2, 2);
+	O = A * B;//перемножение матриц
+	cout << "Matrica A*B: " << endl;
+	O.print();
+	matrix E(2, 2);
+	E = ~A;//транспонирование матрицы
+	cout << "Transponir matr A: " << endl;
+	E.print();
+	try // исключение
+	{
+		O = A * X;
+		O.print();
+	}
+	catch (char*& z)
+	{
+		cout << "iskl for * " << z << endl;
+	}
+	cout << endl;
+
+	cout << " di " << value1.get_di() << " mn ";
+	cout << value1.get_mn() << endl;
+	cout << " di " << value2.get_di() << " mn ";
+	cout << value2.get_mn() << endl;
 
 	system("pause");
 	return 0;
 }
-*/
