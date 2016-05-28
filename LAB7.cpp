78de497e8349dde7a317f95b559586379995d01c
Nikki_Moscow <nikk178@mail.ru>
2016-05-28 12:42:27 +0300

    Update LAB7.cpp

--- LAB7.cpp
+++ LAB7.cpp
@@ -1,37 +1,43 @@
 #include <iostream>
 #include <chrono>
+#include <condition_variable>
 #include <thread>
 #include <mutex>
 
 using namespace std;
 using namespace this_thread;
+bool ready = false;
 condition_variable w;
 mutex f;
 void f1(){
     int n=0;
-    while(n<=10){
-        f.lock();
-        for (int i=0; i<4; i++){
-        cout << "aaaaa";
+    unique_lock<mutex> locker(f);
+        for (int i=0; i<10; i++){
+            cout << "aaaaa" << endl;
         sleep_for(chrono::milliseconds(10));
         }
-        f.unlock();
+    ready = true;
+    locker.unlock();
+    w.notify_one();
+    
+    while(n<=10){
+        cout << "aaaaa" << endl;
+        sleep_for(chrono::milliseconds(10));
         n++;
     }
-
 }
+
 void f2(){
     int n=0;
+    unique_lock<mutex> locker(f);
+    
+    w.wait(locker,[]{return ready;});
     while(n<=100){
-        f.lock();
-        for (int i=0;i<4;i++){
-        cout << "bbbbb";
+        cout << "bbbbb" << endl;
         sleep_for(chrono::milliseconds(10));
-        }
-        f.unlock();
         n++;
-        
     }
+    
     
 }
 
