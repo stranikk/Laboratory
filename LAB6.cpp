11cbe4763df157f8d4621f335bf735a85c39df82
Nikki_Moscow <nikk178@mail.ru>
2016-06-05 16:12:57 +0300

    Update LAB6.cpp

--- LAB6.cpp
+++ LAB6.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <math.h>
+#include <cstdlib>
+#include <cmath>
 
 using namespace std;
 
@@ -34,14 +35,26 @@ public:
 		mn = i.mn;
 		return (*this);
 	}
-	compx compx :: operator * (compx &i)// перегрузка произведения 
+	compx & operator=  (const compx &i) { // перегрузка присваивания
+		di = i.di;
+		mn = i.mn;
+		return (*this);
+	}
+	compx operator * (compx &i)// перегрузка произведения 
 	{
 		compx tm;
 		tm.di = di*i.di;
 		tm.mn = di*i.mn;
 		return tm;
 	}
-	compx compx :: operator + (compx &i) // перегрузка сложения
+	compx operator + (compx &i) // перегрузка сложения
+	{
+		compx tm;
+		tm.di = di + i.di;
+		tm.mn = di + i.mn;
+		return tm;
+	}
+	compx operator + (const compx &i) // перегрузка сложения
 	{
 		compx tm;
 		tm.di = di + i.di;
@@ -51,67 +64,28 @@ public:
 };
 template <typename T>
 class matrix {
-	compx** A;
-	T M;
-	T N;
+	T** A;
+	int M;
+	int N;
 public:
-	matrix(T m, T n) : A(NULL), M(m), N(n)
+	matrix<T>(int m, int n) : A(NULL), M(m), N(n)
 	{
-		A = new compx*[M]; //строки в массиве
+		A = new T*[M]; //строки в массиве
 		for (int i = 0; i < M; i++)
 		{
-			A[i] = new compx[N]; //  столбцы
+			A[i] = new T[N]; //  столбцы
 			for (int j = 0; j < N; j++)
-				A[i][j] = compx(0, 0);
+				A[i][j] = T(0, 0);
 		}
 	}
-	compx& operator () (int i, int j) // перегрузка оператора обращение к элементу ()
+	T& operator () (int i, int j) // перегрузка оператора обращение к элементу ()
 	{
 		return A[i][j];
 	}
-	
-	matrix operator+(matrix& M1, matrix& M2)//перегрузка сложения матриц
-	{
-		matrix L(M1.M, M1.N);
-		for (int i = 0; i < M1.M; i++)
-		{
-			for (int j = 0; j < M1.N; j++)
-			{
-				L(i, j) = M2(i, j) + M1(i, j);
-			}
-		}
-		return L;
-	}
-	matrix operator*(matrix& M1, matrix& M2)//перегрузка перемножения матриц
-	{
-		if (M1.N != M2.M)
-			throw "dont true size";
-		matrix Q(M1.M, M1.N);
 
-		for (int i = 0; i < M1.M; i++)
-		{
-			for (int j = 0; j < M1.N; j++)
-			{
-				for (int t = 0; t < M1.N; t++)
-				{
-					Q(i, j) = Q(i, j) + (M1(i, t) * M2(t, j));
-				}
-			}
-		}
-		return Q;
-	}
-	matrix operator~(matrix& M1)//перегрузка транспонирования матрицы
-	{
-		matrix L(M1.N, M1.M);
-		for (int i = 0; i < M1.M; i++)
-		{
-			for (int j = 0; j < M1.N; j++)
-			{
-				L(j, i) = M1(i, j);
-			}
-		}
-		return L;
-	}
+	int m() { return M; };
+	int n() { return N; };
+
 
 	void print()
 	{
@@ -125,19 +99,64 @@ public:
 	}
 
 
-	~matrix()
+	~matrix<T>()
 	{
 
 	}
 
 };
 
+
+template <typename T> matrix<T> operator+(matrix<T>& M1, matrix<T>& M2)//перегрузка сложения матриц
+{
+	matrix<T> L(M1.m(), M1.n());
+	for (int i = 0; i < M1.m(); i++)
+	{
+		for (int j = 0; j < M1.n(); j++)
+		{
+			L(i, j) = M2(i, j) + M1(i, j);
+		}
+	}
+	return L;
+}
+template <typename T>  matrix<T> operator*(matrix<T>& M1, matrix<T>& M2)//перегрузка перемножения матриц
+{
+	if (M1.n() != M2.m())
+		throw "dont true size";
+	matrix<T> Q(M1.m(), M1.n());
+	for (int i = 0; i < M1.m(); i++)
+	{
+		for (int j = 0; j < M1.n(); j++)
+		{
+			for (int t = 0; t < M1.n(); t++)
+			{
+				Q(i, j) = Q(i, j) + (M1(i, t) * M2(t, j));
+			}
+		}
+	}
+	return Q;
+}
+
+template <typename T> matrix<T> operator~(matrix<T>& M1)//перегрузка транспонирования матрицы
+{
+	matrix<T> L(M1.n(), M1.m());
+	for (int i = 0; i < M1.m(); i++)
+	{
+		for (int j = 0; j < M1.n(); j++)
+		{
+			L(j, i) = M1(i, j);
+		}
+	}
+	return L;
+}
+
+
 int main()
 {
 	compx value1(1, 7);
 	compx value2(6, -2);
 
-	matrix <int> A(3, 2);        // До запятой целай часть числа , а потом мнимая
+	matrix <compx> A(3, 2);        // До запятой целай часть числа , а потом мнимая
 	A(0, 0) = compx(2, 3);
 	A(0, 1) = compx(1, 1);
 	A(1, 0) = compx(0, 1);
@@ -145,13 +164,13 @@ int main()
 	A(2, 0) = compx(0, 0);
 	A(2, 1) = compx(1, 3);
 
-	matrix <int> B(2, 2);
+	matrix <compx> B(2, 2);
 	B(0, 0) = compx(1, 5);
 	B(0, 1) = compx(3, 3);
 	B(1, 0) = compx(6, 1);
 	B(1, 1) = compx(1, 0);
 
-	matrix <int> X(3, 2);
+	matrix <compx> X(3, 2);
 	X(0, 0) = compx(2, 2);
 	X(0, 1) = compx(1, 3);
 	X(1, 0) = compx(7, 3);
@@ -165,15 +184,15 @@ int main()
 	B.print();
 	cout << "Matrica X: " << endl;
 	X.print();
-	matrix <int> C(2, 2);
+	matrix <compx> C(2, 2);
 	C = A + X;//сложение матриц
 	cout << "Matrica A+F: " << endl;
 	C.print();
-	matrix <int> O(2, 2);
+	matrix <compx> O(2, 2);
 	O = A * B;//перемножение матриц
 	cout << "Matrica A*B: " << endl;
 	O.print();
-	matrix <int> E(2, 2);
+	matrix <compx> E(2, 2);
 	E = ~A;//транспонирование матрицы
 	cout << "Transponir matr A: " << endl;
 	E.print();
@@ -182,7 +201,7 @@ int main()
 		O = A * X;
 		O.print();
 	}
-	catch (char*& z)
+	catch (const char * z)
 	{
 		cout << "iskl for * " << z << endl;
 	}
